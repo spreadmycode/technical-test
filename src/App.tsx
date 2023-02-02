c99f8992909ca8d185711e808dddd2c90422baf4
spreadmycode <spreadmycode@outlook.com>
2023-02-02 08:44:10 -0800

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MapboxView from "./MapboxView";
+
+function App() {
+  return (
+    <div className="App">
+      <MapboxView />
+    </div>
+  );
+}
+
+export default App;
